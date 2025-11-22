add852283552468b4fc00b9c6679c10789c83ed3
20ljh <32203502@dankook.ac.kr>
2025-11-23 00:01:51 +0900

    백엔드 서버 파일 수정 - 백엔드 서버 내부 주소 출력부 추가

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 // backend/server.js
 const http = require('http');
+const os = require('os'); // OS 정보를 가져오는 모듈
+
+// 컨테이너의 내부 IP 주소를 찾는 함수
+function getServerIP() {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            // IPv4이고, 내부 주소(127.0.0.1)가 아닌 것을 찾음
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return 'Unknown IP';
+}
 
 const server = http.createServer((req, res) => {
-    console.log("백엔드 서버에 요청이 도착했습니다! (5초 지연 시작...)");
+    console.log(`백엔드 서버에 요청이 도착했습니다! (5초 지연 시작...)`);
     
     // 5초(5000ms) 동안 멈췄다가 응답을 보냄
     setTimeout(() => {
@@ -12,5 +27,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(80, () => {
-    console.log('느린 백엔드 서버가 80번 포트에서 대기 중입니다.');
+    const ip = getServerIP(); // IP 가져오기
+    console.log(`느린 백엔드 서버가 80번 포트에서 대기 중입니다.`);
+    console.log(`백엔드 서버 내부 IP 주소: ${ip}`);
 });
